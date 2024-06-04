4fc343b6784636879eca2711ae5b0bd9ab3eaf2e
MathildaRemy <169890337+MathildaRemy@users.noreply.github.com>
2024-06-04 15:23:00 +0200

    Comments and dockstring added in preprocessing

--- resstore-mri-dwi/preprocessing.py
+++ resstore-mri-dwi/preprocessing.py
@@ -2,31 +2,40 @@
 """
 Functions for preprocessing DWI data:
     - get_dwifslpreproc_command
-    - run_preproc_dwi
-
+    - run_preproc_dwi: perform preprocessing and optionnal FOD 
 """
 
-import logging
 import os
 
 from FOD import FOD
 from useful import check_file_ext, convert_mif_to_nifti, execute_command
 
-# Set up logging, to check
-logging.basicConfig(level=logging.INFO)
-mylog = logging.getLogger(__name__)
-
 EXT = {"NIFTI_GZ": "nii.gz", "NIFTI": "nii"}
+
+
+
+
 
 
 def get_dwifslpreproc_command(
     in_dwi, dwi_out, pe_dir, readout_time, b0_pair=None, rpe=None, shell=False
 ):
     """
-    Get dwifslpreproc command.
+    Get dwifslpreproc command
+
+    Parameters: 
+    - in_dwi: input file .mif format to be corrected
+    - dwi_out: file that will contain the motion and distortion corrected images
+    - pe_dir: phase encoding direction
+    - readout_time: the time it takes to acquire the k-space data after the excitation pulse (s)
+    - b0_pair: (optionnal) b0_pair file in .mif format 
+    - rpe: (optionnal) ratio of partial echo = fraction of the k-space that are encoded
+    - shell: (default:False) no info was given on the b-values
+
+    Retun:
+    - command: to run the dwifslpreproc 
     """
     command = ["dwifslpreproc", in_dwi, dwi_out]
-
     if not rpe:
         command += [
             "-rpe_none",
@@ -60,27 +69,49 @@ def get_dwifslpreproc_command(
     return command
 
 
+
+
+
+
+
+
 def run_preproc_dwi(
     in_dwi, pe_dir, readout_time, rpe=None, shell=True, in_pepolar_PA=None, in_pepolar_AP=None
 ):
     """
-    Run preproc for whole brain diffusion using MRtrix command
+    Run preproc for whole brain diffusion using MRtrix command and an optionnal FOD estimation 
+
+    Parameters:
+    - in_dwi: input file .mif format to be preprocessed
+    - pe_dir: phase encoding direction
+    - readout_time: the time it takes to acquire the k-space data after the excitation pulse (s)
+    - rpe: (optionnal) ratio of partial echo = fraction of the k-space that are encoded
+    - shell: (default:True) info was given on the b-values
+    - in_pepolar_PA: (optionnal), fmap PA in .mif format
+    - in_pepolar_AP: (optionnal), fmap AP in .mif format
+
+    Returns: 
+    - int 1 success, 0 failure 
+    - msg 
+    - info
     """
 
+    # Ask user for FOD 
     user_input = input(f"Do you want to perform FOD estimation? (yes/no): ").strip().lower()
 
-    info = {}
     # Get files name
+    info = {}
     dir_name = os.path.dirname(in_dwi)
     valid_bool, in_ext, file_name = check_file_ext(in_dwi, {"MIF": "mif"})
 
     # Denoise
     dwi_denoise = os.path.join(dir_name, file_name + "_denoise.mif")
+    # Check if the file already exists or not
     if not os.path.exists(dwi_denoise):
         cmd = ["dwidenoise", in_dwi, dwi_denoise]
         result, stderrl, sdtoutl = execute_command(cmd)
         if result != 0:
-            msg = f"Can not lunch mrdegibbs (exit code {result})"
+            msg = f"Can not lunch dwidenoise (exit code {result})"
             return 0, msg, info
         else:
             print(f"Denoise completed. Output file: {dwi_denoise}")
@@ -89,6 +120,7 @@ def run_preproc_dwi(
 
     # DeGibbs / Unringing
     dwi_degibbs = dwi_denoise.replace("_denoise.mif", "_denoise_degibbs.mif")
+    # Check if the file already exists or not
     if not os.path.exists(dwi_degibbs):
         cmd = ["mrdegibbs", dwi_denoise, dwi_degibbs]
         result, stderrl, sdtoutl = execute_command(cmd)
@@ -102,10 +134,12 @@ def run_preproc_dwi(
 
     # Motion and distortion correction
     dwi_preproc = dwi_degibbs.replace("_degibbs.mif", "_degibbs_preproc.mif")
+    # Check if the file already exists or not
     if not os.path.exists(dwi_preproc):
 
         # Create b0_pair 
         b0_pair = os.path.join(dir_name, "b0_pair.mif")
+        # Check if the file already exists or not
         if not os.path.exists(b0_pair):
             # Create command for b0_pair creation if pe_dir=PA
             if pe_dir=="j" and in_pepolar_AP:
@@ -113,6 +147,7 @@ def run_preproc_dwi(
                 if in_pepolar_PA==None:
                     # Extract b0 (PA) from dwi 
                     in_pepolar_PA = in_dwi.replace(".mif", "_bzero.mif")
+                    # Check if the file already exists or not
                     if not os.path.exists(in_pepolar_PA):
                         cmd = ["dwiextract", in_dwi, in_pepolar_PA, "-bzero"]
                         result, stderrl, sdtoutl = execute_command(cmd)
@@ -131,6 +166,7 @@ def run_preproc_dwi(
                 if in_pepolar_AP==None:
                 # Extract b0 (AP) from dwi 
                     in_pepolar_AP = in_dwi.replace(".mif", "_bzero.mif")
+                    # Check if the file already exists or not
                     if not os.path.exists(in_pepolar_AP):
                         cmd = ["dwiextract", in_dwi, in_pepolar_AP, "-bzero"]
                         result, stderrl, sdtoutl = execute_command(cmd)
@@ -154,7 +190,6 @@ def run_preproc_dwi(
         else:
             print(f"Skipping b0_pair creation step, {b0_pair} already exists.")
             
-        # Motion distortion correction
         # fslpreproc (topup and Eddy)
         if b0_pair:
             cmd = get_dwifslpreproc_command(
@@ -180,9 +215,11 @@ def run_preproc_dwi(
     else:
         print(f"Skipping motion correction step, {dwi_preproc} already exists.")
 
+
     # Bias correction
     dwi_unbias = dwi_preproc.replace("_degibbs_preproc.mif", "_degibbs_preproc_unbiased.mif")
     bias_output = dwi_preproc.replace("_degibbs_preproc.mif", "_degibbs_preproc_bias.mif")
+    # Check if the file already exists or not
     if not os.path.exists(dwi_unbias) and not os.path.exists(bias_output):
         cmd = ["dwibiascorrect", "ants", dwi_preproc, dwi_unbias,"-bias", bias_output]
         result, stderrl, sdtoutl = execute_command(cmd)
@@ -197,6 +234,7 @@ def run_preproc_dwi(
 
     # Brain mask
     dwi_mask = dwi_unbias.replace("_degibbs_preproc_unbiased.mif", "_dwi_brain_mask.mif")
+    # Check if the file already exists or not
     if not os.path.exists(dwi_mask):
         cmd = ["dwi2mask", dwi_unbias, dwi_mask]
         result, stderrl, sdtoutl = execute_command(cmd)
@@ -217,5 +255,3 @@ def run_preproc_dwi(
     msg = "Preprocessing DWI done"
     print(msg)
     return 1, msg, info
-
-    
