76d232dccd911fd7ebc72d8916397b502c7ad688
MathildaRemy <169890337+MathildaRemy@users.noreply.github.com>
2024-06-05 12:31:37 +0200

    Update name of the files created by FOD function

--- resstore-mri-dwi/FOD.py
+++ resstore-mri-dwi/FOD.py
@@ -31,10 +31,10 @@ def FOD(in_dwi, mask):
     valid_bool, in_ext, file_name = check_file_ext(in_dwi, {"MIF": "mif"})
     
     # RF estimation 
-    voxels = os.path.join(dir_name, "voxels.mif")
-    wm = os.path.join(dir_name, "wm.txt")
-    gm = os.path.join(dir_name, "gm.txt")
-    csf = os.path.join(dir_name, "csf.txt")
+    voxels = os.path.join(dir_name, file_name + "_voxels.mif")
+    wm = os.path.join(dir_name, file_name + "_wm.txt")
+    gm = os.path.join(dir_name, file_name + "_gm.txt")
+    csf = os.path.join(dir_name, file_name + "_csf.txt")
     if not os.path.exists(voxels):
         cmd = ["dwi2response", "dhollander", in_dwi, wm, gm, csf, "-voxels", voxels]
         result, stderrl, sdtoutl = execute_command(cmd)
@@ -47,10 +47,10 @@ def FOD(in_dwi, mask):
         print(f"Skipping RF estimation step, {voxels} already exists.")
 
     # FOD estimation 
-    vf = os.path.join(dir_name, "vf.mif")
-    wmfod = os.path.join(dir_name, "wmfod.mif")
-    gmfod = os.path.join(dir_name, "gmfod.mif")
-    csffod = os.path.join(dir_name, "csffod.mif")
+    vf = os.path.join(dir_name, file_name + "_vf.mif")
+    wmfod = os.path.join(dir_name, file_name + "_wmfod.mif")
+    gmfod = os.path.join(dir_name, file_name + "_gmfod.mif")
+    csffod = os.path.join(dir_name, file_name + "_csffod.mif")
     if not os.path.exists(vf) :
         if not (os.path.exists(wmfod) and os.path.exists(gmfod) and os.path.exists(csffod)):
             cmd = ["dwi2fod", "msmt_csd", in_dwi, "-mask", mask, wm, wmfod, gm, gmfod, csf, csffod]
@@ -86,9 +86,9 @@ def FOD(in_dwi, mask):
         print(f"Skipping FOD estimation step, {vf} already exists.")
 
     # Intensity normalization
-    wmfod_norm = os.path.join(dir_name, "wmfod_norm.mif")
-    gmfod_norm = os.path.join(dir_name, "gmfod_norm.mif")
-    csffod_norm = os.path.join(dir_name, "csffod_norm.mif")
+    wmfod_norm = os.path.join(dir_name, file_name + "_wmfod_norm.mif")
+    gmfod_norm = os.path.join(dir_name, file_name + "_gmfod_norm.mif")
+    csffod_norm = os.path.join(dir_name, file_name + "_csffod_norm.mif")
     if not (os.path.exists(wmfod_norm) and os.path.exists(gmfod_norm) and os.path.exists(csffod_norm)):
         cmd = ["mtnormalise", wmfod, wmfod_norm, gmfod, gmfod_norm, csffod, csffod_norm, "-mask", mask]
         result, stderrl, sdtoutl = execute_command(cmd)
