2dc7001abdd853e8dd9550bb152293f28da33e0e
MathildaRemy <169890337+MathildaRemy@users.noreply.github.com>
2024-07-23 11:58:04 +0200

    New add data for one subject only

--- resources/add_data_subject.py
+++ resources/add_data_subject.py
@@ -17,10 +17,18 @@ if __name__ == '__main__':
     parser.add_argument(
         '-c', '--config_file', required=True, help='dcm2bids config file'
     )
+    parser.add_argument(
+        '-subject', required=True, help='subject id (ex: 075 or 003)'
+    )
+    parser.add_argument(
+        '-visit', required=True, help='visit id (ex: 01 or 02)'
+    )
     args = parser.parse_args()
     sourcedata = args.sourcedata
     output_directory = args.output
     config_file = args.config_file
+    sub_id = args.subject
+    visit_id = args.visit
 
     # Trouver tous les dossiers de sujets dans fichier source
     all_subject_folders = glob.glob(os.path.join(sourcedata, 'subject-*'))
@@ -47,11 +55,13 @@ if __name__ == '__main__':
             # Récupère le numéro du sujet 
             id_sub = os.path.basename(subject_folder).split('subject-')[-1]
             print('\nIdentifiant du sujet :', id_sub)
+            if id_sub != sub_id:
+                continue
             
             # Trouver tous les dossiers de visite pour ce sujet dans le fichier source 
             visit_folders = glob.glob(os.path.join(subject_folder, 'visit-*'))
 
-            # Verfier si le fichier source contient des visites pour ce sujet
+            # S'il n'y a pas de visite on continue 
             if not visit_folders:
                 print(f"Aucun dossier de visite trouvé pour le sujet {id_sub}.")
                 continue
@@ -64,8 +74,10 @@ if __name__ == '__main__':
 
                     # Recupère le numéro de le session
                     id_ses = os.path.basename(visit_folder).split('visit-')[-1]
+                    if id_ses != visit_id:
+                        continue
 
-                    # Si le dossier de sortie est vide, on traite directement les visites
+                    # Si le dossier de sortie est vide, on traite directement la visite concernée
                     if not all_subject_folders_out:
                         # Construire la commande dcm2bids
                         cmd = f'dcm2bids -d {visit_folder} -p {id_sub} -s {id_ses} -c {config_file} -o {output_directory}'
@@ -82,6 +94,11 @@ if __name__ == '__main__':
                         # Vérifier si le répertoire est valide
                         if os.path.isdir(subject_folder_out):
 
+                            # Récupère le numéro du sujet 
+                            id_sub_out = os.path.basename(subject_folder_out).split('sub-')[-1]
+                            if id_sub_out != sub_id:
+                                continue
+
                             # Trouver tous les dossiers de visite pour ce sujet dans le fichier source 
                             visit_folders_out = glob.glob(os.path.join(subject_folder_out, 'ses-*'))
 
@@ -92,8 +109,9 @@ if __name__ == '__main__':
                                 if os.path.isdir(visit_folder_out):
                     
                                     # Vérifier si le dossier de session correspond à l'identifiant id_ses
-                                    if visit_folder_out.startswith(f'ses-{id_ses}'):
-                                        continue
+                                    if visit_folder_out.startswith(f'ses-{visit_id}'):
+                                        print(f'La session {visit_id} du sujet {id_sub_out} a déjà été traitée')
+                                        exit()
 
                             # Arrivé ici cela signifie que le fichier visite n'a pas encore été traité donc on le fait  
                             # Construire la commande dcm2bids
